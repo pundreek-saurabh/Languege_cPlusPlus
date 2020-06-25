9af06dae0349f5bbb08690ba5b024159cd86659d
Pundreek Saurabh Srivastava <49810048+pundreek-saurabh@users.noreply.github.com>
2020-06-25 19:40:33 +0530

    Add files via upload

---
+++ call_by_value.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void change(int no)
+{
+    no=no*no;
+    cout<<"value in function: "<<no<<endl;
+}
+int main()
+{
+    int n; 
+    cout<<"enter the value:"<<endl;
+    cin>>n;
+    change (n);
+    cout<<"value in main : "<<n;
+    return 0;
+}
