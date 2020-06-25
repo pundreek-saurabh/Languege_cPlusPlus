386261c5e7eb5851ab026156a707c4f2f8f27054
Pundreek Saurabh Srivastava <49810048+pundreek-saurabh@users.noreply.github.com>
2020-06-25 20:20:21 +0530

    Add files via upload

---
+++ array_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[10],i,j;
+    for(i=1;i<=10;i++)
+    {
+        cin>>array[i];
+    }
+    cout<<array[10];
+    return 0;
+}
+//solve it.
